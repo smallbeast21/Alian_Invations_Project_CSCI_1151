869cfc4a519827dc729122f601c12758b490f1f2
Ishmael Kaloko <ishmaelkaloko@gmail.com>
2025-04-13 20:09:16 -0400

    New PNG FIles for Enemy and Lasers

--- settings.py
+++ settings.py
@@ -10,15 +10,34 @@ class Settings:
         #Had to use tis path style that takes the parent of the script(Folder)
          #and goes through that path to pull the assets or anything else
          #because the one from your video would not work me
+
+
+        #BG
         self.bg_file = \
         Path(__file__).parent / 'Assets' / 'images' / 'SpacePixalBackground.png'
+        
+        #Ship
         self.ship_file = \
         Path(__file__).parent / 'Assets' / 'images' / 'YellowShip.png'
+
+        #Space Ship
+        self.enemy_file = \
+        Path(__file__).parent / 'Assets' / 'images' / 'ememyShip.png'
+
+        #Bullet\
+        self.bullet_file = \
+        Path(__file__).parent / 'Assets' / 'images' / 'WhiteDualLaser.png'
+
+
         
         # Ship settings
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
+
+        #Settings
+        self.enemy_speed = 2
+        self.bullet_speed = 10
         
         # Which border to start on: 'left' or 'right'
-        self.ship_side = 'left'
+        self.ship_side = 'right'
